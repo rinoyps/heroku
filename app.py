f4024e09809971d014090b2f4a11f765c5ecc0b4
rinoyps <rinoy@iiflcap.com>
2021-11-13 14:06:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+import joblib
+from flask import Flask,render_template, request
+
+
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+ 
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+
+@app.route('/home')
+# ‘/’ URL is bound with app
+def home():
+    return render_template('index.html')
+   
+@app.route('/predict' , methods = ['POST']) 
+
+def predict():
+    fname = request.form.get('firstname')
+    lname = request.form.get('lastname')
+    email = request.form.get('mail')
+    phone = request.form.get('phonenumber')
+
+    print(fname , lname , email , phone)
+    
+    return render_template('index.html')
+
+prediction=loaded_model=joblib.load('dib_78.pkl')
+
+if prediction==1:
+    val='diabetic'
+else:
+    val='no diabetic'
+
+    print(val)
+
+    #return render_template('homepage.html',val)
+
+#print(loaded_model.predict([[10,20,30,4,5,6,8,9]]))
+
+def __getitem__(self,index):
+    return self.bricks.bricksId[index]
+
+def __setitem__(self,index,value):
+    self.bricks.bricksId[index] = value
+
+
+   # main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
